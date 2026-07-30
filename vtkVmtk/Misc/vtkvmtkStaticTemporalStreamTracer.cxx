ed213a5efb343f703ff0d51549464325ce3ad82a
Andras Lasso <lasso@queensu.ca>
2026-07-30 09:56:31 -0400

    Fix crash on uninitialized pointer in StaticTemporalStreamTracer
    CheckInputs can fail before assigning the velocity field interpolator (no input data, or empty composite input), in which case RequestData called Delete() on an uninitialized pointer. Initialize the pointer to null and guard the Delete call.

--- vtkVmtk/Misc/vtkvmtkStaticTemporalStreamTracer.cxx
+++ vtkVmtk/Misc/vtkvmtkStaticTemporalStreamTracer.cxx
@@ -253,12 +253,15 @@ int vtkvmtkStaticTemporalStreamTracer::RequestData(
   if (seeds)
     {
     double lastPoint[3];
-    vtkAbstractInterpolatedVelocityField * func;
+    vtkAbstractInterpolatedVelocityField * func = 0;
     int maxCellSize = 0;
     if (this->CheckInputs(func, &maxCellSize) != VTK_OK)
       {
       vtkDebugMacro("No appropriate inputs have been found. Can not execute.");
-      func->Delete();
+      if (func)
+        {
+        func->Delete();
+        }
       seeds->Delete();
       startTimes->Delete();
       integrationDirections->Delete();
